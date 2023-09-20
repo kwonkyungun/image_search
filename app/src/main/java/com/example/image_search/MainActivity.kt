dfd2b1d0f25d2a4c3da09320b1415f1558a48bc5
kku <ruddns5315@gmail.com>
2023-09-20 16:56:07 +0900

    [FEAT] : ⚙️: 탭 레이아웃 추가

--- app/src/main/java/com/example/image_search/MainActivity.kt
+++ app/src/main/java/com/example/image_search/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.image_search
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.viewpager2.widget.ViewPager2
+import com.example.image_search.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var tab1: Fragment_home
+    lateinit var tab2: Fragment_locker
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViewPager()
+
+
+    }
+    private fun initViewPager() {
+        var viewPager2Adatper = ViewPager2Adapter(this)
+        viewPager2Adatper.addFragment(Fragment_home())
+        viewPager2Adatper.addFragment(Fragment_locker())
+
+        binding.mainViewpager.apply {
+            adapter = viewPager2Adatper
+
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                }
+            })
+        }
+
+        TabLayoutMediator(binding.mainTabLayout, binding.mainViewpager) { tab, position ->
+            Log.e("YMC", "ViewPager position: ${position}")
+            when (position) {
+                0 -> tab.text = "간식결과"
+                1 -> tab.text = "내 보관함"
+            }
+        }.attach()
     }
 }
